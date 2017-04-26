661417ed46eb9fb0f770d02828e47054edcb0da4
Luc Flynn <lucnmsu@gmail.com>
2017-04-26 11:33:28 -0600

    Done with milestone-1

--- use-case.php
+++ use-case.php
@@ -5,7 +5,15 @@
 		<title>use-case</title>
 	</head>
 
-	<main>
-		<h1> Hello use-case</h1>
-	</main>
+	<body>
+		<h1> Emily, like the vast majority of her contemporaries checks her phone constantly. She loves instagram and twitter and checks them multiple times throughout the day to catch up on her favorite content providers. Anytimes she takes a break she pulls out her phones, checks text messages on her phone then flips over to instagram and twitter. She likes to see people who are accomplishing goals, and working hard for what they want. This motivates her to keep working on her life to achieve her dreams. She would use my profile as a gateway to my instagram and twitter so she could follow me and I could become one of her favorite content creators.  </h1>
+
+		<h2> Interaction Flow</h2>
+		<ol>
+			<li> Emily will find me on google/li>
+			<li> PWP opens to her from goodle</li>
+			<li> Emily will be dazzled by my cool pictures and life, become curious to find out more.</li>
+			<li> Emily will scroll up and down on my page, eventually clicking on the link to my instagram page. </li>
+		</ol>
+	</body>
 </html>
