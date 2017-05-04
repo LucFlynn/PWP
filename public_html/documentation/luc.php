38c5432ff9b1fa05a12fc3e7abba1591f0fcca4b
Luc Flynn <lucnmsu@gmail.com>
2017-05-04 10:12:18 -0600

    Began work on main page.

---
+++ public_html/documentation/luc.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<!-- PWp Main layout
+  -- Author Luc Flynn -->
+
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link type="text/css" href="css/style.css" rel="stylesheet" />
+    <title> Luc Flynn </title>
+</head>
+
+<main>
+    <h1>Luc Flynn</h1>
+    <em> Welcome to my personal website</em>
+</main>
+
+<body>
+    <h2> About me </h2>
+
+    <div class ="goal">
+        <img src="css/goal.jpeg">
+    </div>
+        <p> Fanny pack 8-bit thundercats, pinterest single-origin coffee ennui squid deep v flannel. Occupy lo-fi pabst, tacos tote bag kombucha cred hammock typewriter iceland succulents roof party man braid authentic scenester. Austin cronut actually typewriter, live-edge trust fund waistcoat mlkshk lomo small batch. Hot chicken salvia flannel, raw denim truffaut dreamcatcher hell of tumeric fanny pack trust fund cronut vice. Tote bag kickstarter meggings, activated charcoal thundercats raw denim lumbersexual austin heirloom mixtape knausgaard hexagon keytar bushwick. Occupy williamsburg cred coloring book crucifix marfa yr. Iceland fam poke, seitan intelligentsia actually subway tile ugh activated charcoal sustainable.
+
+            Fixie banjo etsy, brooklyn health goth squid meh vaporware marfa drinking vinegar chartreuse jean shorts lomo leggings helvetica. Coloring book listicle wolf, plaid hexagon mlkshk sustainable mustache tattooed gentrify small batch yr cred farm-to-table. Venmo pok pok craft beer, flannel edison bulb dreamcatcher tousled bicycle rights. Jean shorts flexitarian viral, vegan DIY narwhal lyft jianbing typewriter stumptown cliche truffaut. Meh deep v cornhole vape lo-fi semiotics. Tattooed synth pabst kinfolk snackwave, hella schlitz raclette kale chips fanny pack neutra. Drinking vinegar air plant kickstarter meh ramps.</p>_
+
+            <p> Cornhole wayfarers sriracha edison bulb slow-carb gastropub live-edge farm-to-table subway tile banh mi drinking vinegar, snackwave roof party brunch. Mumblecore tote bag 8-bit pinterest taxidermy, quinoa mixtape poke chartreuse. Etsy four loko literally cornhole, tacos wolf farm-to-table actually banh mi subway tile lo-fi kombucha vice cliche single-origin coffee. Hashtag williamsburg fingerstache pickled poutine hexagon hoodie leggings meh snackwave skateboard, iPhone taxidermy truffaut. Kickstarter raw denim activated charcoal, direct trade heirloom taxidermy whatever +1 la croix ramps woke. Umami lyft direct trade fap shabby chic, ugh farm-to-table godard bicycle rights beard. Kogi iPhone beard pop-up chia.
+
+            Succulents etsy marfa tousled, trust fund before they sold out edison bulb hoodie enamel pin live-edge hot chicken. Aesthetic post-ironic hexagon, PBR&B thundercats viral godard fingerstache irony tofu succulents ethical hell of pok pok. Portland flannel sustainable, forage iPhone woke banh mi hexagon schlitz bespoke. Brunch poutine flannel fashion axe. Actually thundercats quinoa, twee chia locavore glossier tumeric hot chicken. Unicorn everyday carry austin cornhole. Helvetica pabst flannel, readymade chicharrones heirloom unicorn schlitz.
+            Oh. You need a little dummy text for your mockup? How quaint.
+
+            I bet you’re still using Bootstrap too… </p>
+</body>
+
+
+</html>
--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,29 +16,33 @@
             <em>Milestone 1</em>
 	</main>
 	<body>
-
+    <div class ="goal">
+        <img src="css/goal.jpeg">
+    </div>
         <div class ="PAG" >
-            <div class ="goal">
-                <img src="css/goal.jpeg">
-            </div>
+
         <p> <strong>Purpose:</strong> <br> To drive people, employers, clients, fans, to my social media sites so I can gain followers.</p>
         <p> <strong>Audience:</strong> <br> Will be fans of motivational social media accounts, clients/employers looking to create large social media followings.</p>
         <p> <strong>Goal:</strong> <br> To intice people to follow me, and send me inquires about creating similar frameworks for their business. Explicitly I want messages like "I saw your website and had to follow/send you a message."</p>
         </div>
+
         <div class ="persona">
+
         <h2>Persona</h2>
+
         <div class ="emily">
-        <img src="css/emily.jpeg">      </div>
-		<p> <strong>Name</strong> Emily <br> <strong>Age</strong> 24</p>
-		<p> <strong>Profession</strong> <br> She is a manager at Staples, has worked there for 4 years and started out as a cashier then worked her way up. She mainly sits in the back on the computer looking over paperwork and putting out fires. She's sharper than most of her coworkers and employees so most of her job she has largly found ways to automatic or do very efficiently. She is liked well enough by those that matter in her job but she doesn't pout much effort into making great friends with any of them because it's not in her mind that she'll be where she is in another 4 years.  </p>
-		<p> <strong>Technology</strong> <br> Like most people her age she uses an iphone. Nobody around her really uses android or anything else. She shells out 1,000 every 2 or 3 years for the new latest model. She mainly uses her phone to text her friends, follow people on social media such as instagram, snapchat and facebook. She posts infrequently but when cool stuff happens she posts. She comments much more and likes more than she posts. She has a macbook air, but doesn't use it for much more than netflix, hulu, and pintrest.  </p>
-		<p> <strong>Attitudes and Behaviors</strong> <br> Does what needs to be done regardless of how she feels...but she is more unhappy and unsatisfied than not. She has lots of plans for the future but hasn't made a ton of progress on any of them. SHe just gets up and goes to work even thought she wishes she could get up and live her life. She enjoys working hard but doesn't want her job to feel like a job. She hates the term 'work life balance' because she wants them to literally be one in the same. No balance. Life is work and work is life. Before this however, she needs work she is actually in love with. </p>
-		<p> <strong>Frustrations and Needs</strong> <br> She is frustrated at where her life is. Like most milinials she thought she'd be a milionaire by now. She needs the constant motivation of social media to keep her performing at a high level throughout the day.  </p>
+            <img src="css/emily.jpeg">      </div>
+
+            <p> <strong>Name</strong> Emily <br> <strong>Age</strong> 24</p>
+            <p> <strong>Profession</strong> <br> She is a manager at Staples, has worked there for 4 years and started out as a cashier then worked her way up. She mainly sits in the back on the computer looking over paperwork and putting out fires. She's sharper than most of her coworkers and employees so most of her job she has largly found ways to automatic or do very efficiently. She is liked well enough by those that matter in her job but she doesn't pout much effort into making great friends with any of them because it's not in her mind that she'll be where she is in another 4 years.  </p>
+            <p> <strong>Technology</strong> <br> Like most people her age she uses an iphone. Nobody around her really uses android or anything else. She shells out 1,000 every 2 or 3 years for the new latest model. She mainly uses her phone to text her friends, follow people on social media such as instagram, snapchat and facebook. She posts infrequently but when cool stuff happens she posts. She comments much more and likes more than she posts. She has a macbook air, but doesn't use it for much more than netflix, hulu, and pintrest.  </p>
+            <p> <strong>Attitudes and Behaviors</strong> <br> Does what needs to be done regardless of how she feels...but she is more unhappy and unsatisfied than not. She has lots of plans for the future but hasn't made a ton of progress on any of them. SHe just gets up and goes to work even thought she wishes she could get up and live her life. She enjoys working hard but doesn't want her job to feel like a job. She hates the term 'work life balance' because she wants them to literally be one in the same. No balance. Life is work and work is life. Before this however, she needs work she is actually in love with. </p>
+            <p> <strong>Frustrations and Needs</strong> <br> She is frustrated at where her life is. Like most milinials she thought she'd be a milionaire by now. She needs the constant motivation of social media to keep her performing at a high level throughout the day.  </p>
         </div>
 
     <div class ="use-case">
         <h2> Use Case </h2>
-        <p> Emily, like the vast majority of her contemporaries checks her phone constantly. She loves instagram and twitter and checks them multiple times throughout the day to catch up on her favorite content providers. Anytime she takes a break she pulls out her phone, checks text messages, then flips over to Instagram and twitter. She likes to see people who are accomplishing goals, and working hard for what they want. This motivates her to keep working on her life to achieve her dreams. She would use my profile as a gateway to my instagram and twitter so she could follow me and I could become one of her favorite content creators.  </p>
+            <p> Emily, like the vast majority of her contemporaries checks her phone constantly. She loves instagram and twitter and checks them multiple times throughout the day to catch up on her favorite content providers. Anytime she takes a break she pulls out her phone, checks text messages, then flips over to Instagram and twitter. She likes to see people who are accomplishing goals, and working hard for what they want. This motivates her to keep working on her life to achieve her dreams. She would use my profile as a gateway to my instagram and twitter so she could follow me and I could become one of her favorite content creators.  </p>
 
             <div class="interaction">
         <h2> Interaction Flow</h2>
