40c4b1afd3fc6465e1c60263b05c054e9bd4881b
Luc Flynn <lucnmsu@gmail.com>
2017-05-16 11:17:59 -0600

    added mobile view of wireframe

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,9 +3,9 @@
 
 <img src="css/wireframe.png">
 
-
+<img src="css/mobileview.png">
 <h1> Content Strategy</h1>
-<p> To give a more soulful voice to my expereiences and portfolio that may not be gleaned from my resume. It will start with a pic of me, have a few words, then links to my work in the form of podcasts and images and writings.
+<p> To give a more soulful voice to my experiences and portfolio that may not be gleaned from my resume. It will start with a pic of me, have a few words, then links to my work in the form of podcasts and images and writings.
 </p>
 
 </html>
