02f5cb56e86e4e4b4f85c95a391c5a6044e6e298
Luc Flynn <lucnmsu@gmail.com>
2017-06-02 13:48:00 -0600

    added links and a gallery

--- public_html/index.php
+++ public_html/index.php
@@ -72,7 +72,7 @@
                 <nav class="navbar navbar-inverse">
                     <!-- brand and toggle get grouped for better mobile -->
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-taget="#b-example-navbar-collapse-1" aria-expanded="false">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -84,22 +84,23 @@
                     <!-- collect the nav links, forms and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#">Gallery</a></li>
-                            <li><a href="#">Quora </a></li>
-                            <li><a href="#">Hello World</a></li>
-                            <li><a href="https://https://bootcamp-coders.cnm.edu/~lflynn7/pwp/public_html/index.php" target="_blank">StackOverflow</a> </li>
+                            <li><a href="#rowB" class="scroll link">Gallery</a></li>
+                            <li><a href="#"><i class="fa fa-quora" aria-hidden="true"></i>uora </a></li>
+                            <li><a href="https://www.instagram.com/lucjflynn/"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram </a></li>
+                            <li><a href="https://https://bootcamp-coders.cnm.edu/~lflynn7/pwp/public_html/index.php" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i> StackOverflow</a> </li>
+                            <li><a href="https://github.com/LucFlynn"><i class="fa fa-github" aria-hidden="true"></i> Dat Git Tho </a></li>
                         </ul>
                     </div>    <!--.navbar-collapse -->
                 </nav>
             </div>
         </header>
         
-        <!-- beging main page content -->
+        <!-- begin main page content -->
         <main>
         
         <div class="container">
             
-            <!--Beging welcome row -->
+            <!--Begin welcome row -->
                 <div class="row0">
                     <div class="col-md-12">
                         <div class="jumbotron">
@@ -111,57 +112,97 @@
                     
                 </div>
             
-            <!-- begin 2 column row -->
+            <!-- begin 2 column row -------------->
             <div class="row">
                 <div class="col-md-6">
                     <div class="content-box">
                         <h3> Potrofilio thingy 1</h3>
-                        <p> This is blah blah</p>
+                        <p> Ethical chia unicorn raw denim pop-up air plant, ramps snackwave XOXO pork belly freegan dreamcatcher. Ugh everyday carry occupy before they sold out williamsburg kombucha cardigan pitchfork waistcoat. Hexagon ugh green juice, microdosing adaptogen vinyl mixtape next level butcher plaid cred four loko banjo. Flannel cornhole gastropub XOXO put a bird on it hoodie. Cred offal leggings, cold-pressed irony street art succulents you probably haven't heard of them subway tile direct trade retro marfa pitchfork poke. Food truck fap kogi cloud bread. Hexagon tumeric butcher semiotics PBR&B, selfies echo park wayfarers schlitz.
+
+                            Mustache wolf polaroid, whatever echo park paleo bespoke cliche tbh. Coloring book af roof party taiyaki kickstarter XOXO etsy narwhal. IPhone offal yr skateboard, semiotics letterpress you probably haven't heard of them direct trade prism craft beer street art typewriter umami. Scenester try-hard vaporware roof party, gastropub messenger bag chartreuse actually gentrify trust fund mumblecore ugh hexagon tousled. Vice hexagon you probably haven't heard of them affogato gastropub, kogi four dollar toast meh biodiesel af. Occupy butcher gastropub hell of, subway tile tattooed tousled hammock lyft pork belly pitchfork mumblecore wolf. Lyft scenester chambray meh twee.
+
+                            Put a bird on it vegan fashion axe 90's schlitz bespoke. Irony poutine iceland, snackwave kinfolk man bun shoreditch asymmetrical beard cred. Bitters portland readymade vegan, austin chicharrones plaid 8-bit mlkshk lomo. Hashtag snackwave seitan, meh lyft kickstarter crucifix biodiesel. Kitsch pickled four loko pitchfork, small batch banh mi echo park kogi stumptown. Normcore swag man bun cliche, bushwick retro pug wolf post-ironic copper mug pinterest everyday carry jean shorts celiac plaid. Paleo drinking vinegar man braid, activated charcoal portland 8-bit gluten-free fam.</p>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="content-box">
                         <h3> flexbox whatever</h3>
-                        <p> More words </p>
+                        <p> Ethical chia unicorn raw denim pop-up air plant, ramps snackwave XOXO pork belly freegan dreamcatcher. Ugh everyday carry occupy before they sold out williamsburg kombucha cardigan pitchfork waistcoat. Hexagon ugh green juice, microdosing adaptogen vinyl mixtape next level butcher plaid cred four loko banjo. Flannel cornhole gastropub XOXO put a bird on it hoodie. Cred offal leggings, cold-pressed irony street art succulents you probably haven't heard of them subway tile direct trade retro marfa pitchfork poke. Food truck fap kogi cloud bread. Hexagon tumeric butcher semiotics PBR&B, selfies echo park wayfarers schlitz.
+
+                            Mustache wolf polaroid, whatever echo park paleo bespoke cliche tbh. Coloring book af roof party taiyaki kickstarter XOXO etsy narwhal. IPhone offal yr skateboard, semiotics letterpress you probably haven't heard of them direct trade prism craft beer street art typewriter umami. Scenester try-hard vaporware roof party, gastropub messenger bag chartreuse actually gentrify trust fund mumblecore ugh hexagon tousled. Vice hexagon you probably haven't heard of them affogato gastropub, kogi four dollar toast meh biodiesel af. Occupy butcher gastropub hell of, subway tile tattooed tousled hammock lyft pork belly pitchfork mumblecore wolf. Lyft scenester chambray meh twee.
+
+                            Put a bird on it vegan fashion axe 90's schlitz bespoke. Irony poutine iceland, snackwave kinfolk man bun shoreditch asymmetrical beard cred. Bitters portland readymade vegan, austin chicharrones plaid 8-bit mlkshk lomo. Hashtag snackwave seitan, meh lyft kickstarter crucifix biodiesel. Kitsch pickled four loko pitchfork, small batch banh mi echo park kogi stumptown. Normcore swag man bun cliche, bushwick retro pug wolf post-ironic copper mug pinterest everyday carry jean shorts celiac plaid. Paleo drinking vinegar man braid, activated charcoal portland 8-bit gluten-free fam.
+
+                            Put a bird on it vegan fashion axe 90's schlitz bespoke. Irony poutine iceland, snackwave kinfolk man bun shoreditch asymmetrical beard cred. Bitters portland readymade vegan, austin chicharrones plaid 8-bit mlkshk lomo. Hashtag snackwave seitan, meh lyft kickstarter crucifix biodiesel. Kitsch pickled four loko pitchfork, small batch banh mi echo park kogi stumptown. Normcore swag man bun cliche, bushwick retro pug wolf post-ironic copper mug pinterest everyday carry jean shorts celiac plaid. Paleo drinking vinegar man braid, activated charcoal portland 8-bit gluten-free fam.
+
+                            Put a bird on it vegan fashion axe 90's schlitz bespoke. Irony poutine iceland, snackwave kinfolk man bun shoreditch asymmetrical beard cred. Bitters portland readymade vegan, austin chicharrones plaid 8-bit mlkshk lomo. Hashtag snackwave seitan, meh lyft kickstarter crucifix biodiesel. Kitsch pickled four loko pitchfork, small batch banh mi echo park kogi stumptown. Normcore swag man bun cliche, bushwick retro pug wolf post-ironic copper mug pinterest everyday carry jean shorts celiac plaid. Paleo drinking vinegar man braid, activated charcoal portland 8-bit gluten-free fam.</p>
                     </div>
                 </div>
             </div>
 
-            <!-- begin 4 colo row -->
-            <div class="rowA">
-                <div class="col-md-3">
-                    <div class="content-box">
-                        <h3> Navbar </h3>
-                        <p> This example uses bootsrap built in navbar RTFM here: </p>
-                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3">
-                    <div class="content-box">
-                        <h3>Flexbox sticky footer</h3>
-                        <p> Keep the footer at the bottom of the page, always. even if the content are is "short" This is the finest sticky footer I've ever used. </p>
-                    </div>
-                    
-                </div>
-            </div>
+            <!-- begin gallery ------------->
+            <section id="rowB" class="section">
+               <div class="rowB">
+                   <div class="col-xs-6 col-md-3">
+                       <a href="#" class="thumbnail">
+                           <img src="css/images/goal.jpeg" alt="computer">
+                       </a>
+                   </div>
+               
             
+                    <div class="col-xs-6 col-md-3">
+                        <a href="#" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
+                    </div>
+        
+
+                    <div class="col-xs-6 col-md-3">
+                        <a href="#" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
+                    </div>
+    
+
+                    <div class="col-xs-6 col-md-3">
+                        <a href="#" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
+                    </div>
+             </div>
             
-            <div class="rowB">
-                <div class="col-md-3">
-                    <div class="content-box">
-                        <h3> Jumbotron </h3>
-                        <p> The boostrap jumbotron made with "meowh" big or whatever it is sometimes a hero message</p>
+                <!-- begin 2nd row of gallery -->
+                <div class="rowB">
+                    <div class="col-xs-6 col-md-3">
+                        <a href="#" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
+                    </div>
+    
+    
+                    <div class="col-xs-6 col-md-3">
+                        <a href="#" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
+                    </div>
+    
+    
+                    <div class="col-xs-6 col-md-3">
+                        <a href="https://www.instagram.com/p/BUZTuXnhNLR/?taken-by=lucjflynn" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
+                    </div>
+    
+    
+                    <div class="col-xs-6 col-md-3">
+                        <a href="#" class="thumbnail">
+                            <img src="css/images/goal.jpeg" alt="computer">
+                        </a>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="content-box">
-                        <h3> view source</h3>
-                        <p> this is a lotta bootstrap, </p>
-                    </div>
-                </div>
+            
             </div>
-        </div>
+            </section>
         
         </main>
         
@@ -171,60 +212,61 @@
 
 
 <!-- Begin contact form -->
-
-    <form id="contact-form" action="php/mailer.php" method="post">
-    <div class="contact">
-        <div class="form-group">
-            <label for="name">Name <span class="text-danger">*</span></label>
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class="fa fa-user" aria-hidden="true"></i>
+    <div class="col-xs-12 col-md-6 col-lg-6">
+        <form id="contact-form" action="php/mailer.php" method="post">
+        <div class="contact">
+            <div class="form-group">
+                <label for="name">Name <span class="text-danger">*</span></label>
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                    </div>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
                 </div>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
             </div>
-        </div>
-        <div class="form-group">
-            <label for="email">Email <span class="text-danger">*</span></label>
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class="fa fa-envelope" aria-hidden="true"></i>
+            <div class="form-group">
+                <label for="email">Email <span class="text-danger">*</span></label>
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                    </div>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
                 </div>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
             </div>
-        </div>
-        <div class="form-group">
-            <label for="subject">Subject</label>
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class="fa fa-pencil" aria-hidden="true"></i>
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                    </div>
+                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
                 </div>
-                <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
             </div>
-        </div>
-        <div class="form-group">
-            <label for="message">Message</label>
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class="fa fa-comment" aria-hidden="true"></i>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <div class="input-group">
+                    <div class="input-group-addon">
+                        <i class="fa fa-comment" aria-hidden="true"></i>
+                    </div>
+                    <textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"> </textarea>
                 </div>
-                <textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"> </textarea>
             </div>
-        </div>
+        
+            
+            <!-- reCAPTCHA -->
+            <div class="g-recaptcha" data-sitekey="6LfAWyIUAAAAAOFXxCMf2U-Y6KX64cVXjP_k6KJ-"></div>
+            
+            <button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+            <button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+        </form>
     
-        
-        <!-- reCAPTCHA -->
-        <div class="g-recaptcha" data-sitekey="6LfAWyIUAAAAAOFXxCMf2U-Y6KX64cVXjP_k6KJ-"></div>
-        
-        <button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-        <button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-    </form>
-
-<!-- empty area for form error/success output -->
-<div class="row">
-    <div class="col-xs-12">
-        <div id="output-area"></div>
-    </div>
-</div>
+            <!-- empty area for form error/success output -->
+            <div class="row">
+                <div class="col-xs-12">
+                    <div id="output-area"></div>
+                </div>
+            </div>
+         </div>
     </div>
  <!-- sfooter-content-->
     <footer>
